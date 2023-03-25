aaf3097bc2af96492b8c854a6907f9ebda461b8f
Niyati <niyatiagg4641@gmail.com>
2023-03-25 13:49:34 -0700

    Adding basic implementation for encoder

--- src/Encoder.java
+++ src/Encoder.java
@@ -1,8 +1,29 @@
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class Encoder {
     public static List<Integer> encode(String str) {
-        return new LinkedList();
+        int size = 256;
+        final Map<String, Integer> map = new HashMap();
+        for(int i=0; i<size; i++) {
+            map.put(String.valueOf((char)i), i);
+        }
+        final List<Integer> list = new LinkedList();
+        int i=0;
+        while(i<str.length()) {
+            int j=i;
+            while(j < str.length() && map.containsKey(str.substring(i, j+1))) {
+                j++;
+            }
+            list.add(map.get(str.substring(i, j)));
+            if(j < str.length()) {
+                map.put(str.substring(i, j+1), size++);
+            }
+            i=j;
+        }
+
+        return list;
     }
 }
