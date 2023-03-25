f013dde3a64b66193ed4b9f4696ec6a139f7351f
Niyati <niyatiagg4641@gmail.com>
2023-03-25 13:47:37 -0700

    Adding stubs for encoder/decoder

---
+++ src/Decoder.java
@@ -0,0 +1,8 @@
+import java.lang.String;
+import java.util.List;
+
+public class Decoder {
+    public static String decode(List<Integer> list) {
+        return "";
+    }
+}
---
+++ src/Encoder.java
@@ -0,0 +1,8 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Encoder {
+    public static List<Integer> encode(String str) {
+        return new LinkedList();
+    }
+}
